24b4c8cf186cada7016d530e83205e00894bc8f5
parkjjae <wgpark0830@naver.com>
2024-11-20 02:35:49 +0900

    HTTP 데이터를 객체로 처리하는 방법

--- src/main/java/com/sparta/springmvc/request/RequestController.java
+++ src/main/java/com/sparta/springmvc/request/RequestController.java
@@ -40,4 +40,42 @@ public class RequestController {
     public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
     }
+
+
+    // [Request sample]
+// POST http://localhost:8080/hello/request/form/model
+// Header
+//  Content type: application/x-www-form-urlencoded
+// Body
+//  name=Robbie&age=95
+    @PostMapping("/form/model")
+    @ResponseBody
+    public String helloRequestBodyForm(@ModelAttribute Star star) { // body 부분에 들어온 데이터를 객체로 매핑 가능
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/form/param/model?name=Robbie&age=95
+    @GetMapping("/form/param/model")
+    @ResponseBody
+    public String helloRequestParam(@ModelAttribute Star star) { // 객체로 한 번에 받으면 길이가 길어지면 더 수월함.
+        // setter 혹은 오버로딩 된 생성자가 필요함.
+        // ModelAttribute 생략가능
+        // 파라미터가 Simple value type -> int, Date , Integer 면 RequestParam
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+
+    // [Request sample]
+// POST http://localhost:8080/hello/request/form/json
+// Header
+//  Content type: application/json
+// Body
+//  {"name":"Robbie","age":"95"}
+    @PostMapping("/form/json")
+    @ResponseBody
+    public String helloPostRequestJson(@RequestBody Star star) { // HTTP 바디 부분 제이슨 형식으로 들어오면 @RequestBody
+        return String.format("Hello, @RequestBody.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
 }
